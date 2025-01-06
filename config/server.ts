d6a3e89e4e429a380eaa62fb2884aefc27dd1b2c
emb-navenai <emb-navenai@embitel.com>
2025-01-06 10:08:43 +0530

    prod port issue fixes

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,14 @@
+// export default ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'), 
+  port: env.int('PORT'),       
   app: {
     keys: env.array('APP_KEYS'),
   },
